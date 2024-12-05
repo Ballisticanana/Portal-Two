56a07fe27d484d755a75d57bf3794fbfff316fef
Tray-R <tran2501@mpsedu.org>
2024-12-05 09:51:00 -0600

    gonna get players

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.name == "Player1Red" )
+        {
+            Debug.Log("I SEEEEE YOUUUUUUU");
+        }
     }
 }
