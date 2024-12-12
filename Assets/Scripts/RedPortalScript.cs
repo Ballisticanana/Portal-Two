56164af2953f9151dc6e6ec78ec3d9a643e79409
Logan S <loganwseifert@gmail.com>
2024-12-12 10:01:14 -0600

    I hate this rn v2

--- Assets/Scripts/RedPortalScript.cs
+++ Assets/Scripts/RedPortalScript.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class RedPortalScript : MonoBehaviour
@@ -40,11 +41,13 @@ public class RedPortalScript : MonoBehaviour
     public Transform redPortal_Transform;
     public Vector3 redPortal_Position;
     public Quaternion redPortal_Rotation;
+    public Transform redPortal_CameraParent_Transform;
 
     //Blue Portal
     public Transform bluePortal_Transform;
     public Vector3 bluePortal_Position;
     public Quaternion bluePortal_Rotation;
+    public Transform bluePortal_CameraParent_Transform;
     #endregion
 
     void Start()
@@ -88,6 +91,10 @@ public class RedPortalScript : MonoBehaviour
         bluePortal_Transform = GameObject.Find("Blue Portal").GetComponent<Transform>();
         bluePortal_Position = bluePortal_Transform.transform.position;
         bluePortal_Rotation = bluePortal_Transform.transform.rotation;
+
+        redPortal_CameraParent_Transform = GameObject.Find("Red Portals Cameras").transform;
+        bluePortal_CameraParent_Transform = GameObject.Find("Red Portals Cameras").transform;
+
         #endregion
     }
 
@@ -124,12 +131,51 @@ public class RedPortalScript : MonoBehaviour
         bluePortal_Rotation = bluePortal_Transform.transform.rotation;
         #endregion
         #region Manipulate Values
-        player1_ViewFromBlueCamera_Transform.position = bluePortal_Transform.position;
-        Vector3 newRot = bluePortal_Transform.eulerAngles;
-        player1_ViewFromBlueCamera_Transform.eulerAngles = new Vector3(newRot.x, -newRot.y, newRot.z);
-        Vector3 newPos = redPortal_Transform.TransformDirection(player1_Position - redPortal_Position);
-        player1_ViewFromBlueCamera_Transform.localPosition -= new Vector3(newPos.x, newPos.y, newPos.z);
-        //player1_ViewFromBlueCamera_Transform.eulerAngles = player1_Transform.eulerAngles + (bluePortal_Transform.eulerAngles - redPortal_Transform.eulerAngles) + new Vector3(0, 180, 0);
+        player1_ViewFromBlueCamera_Transform.eulerAngles = player1_Transform.eulerAngles + (bluePortal_Transform.eulerAngles - redPortal_Transform.eulerAngles) + new Vector3(0, 180, 0);
+        if (Mathf.Abs(redPortal_Transform.eulerAngles.y % 360) == 90)
+        {
+            player1_ViewFromBlueCamera_Transform.localPosition = -redPortal_Transform.TransformDirection(redPortal_Position - player1_Position);
+            Debug.Log("0 - 90");
+        }
+        else if (Mathf.Abs(redPortal_Transform.eulerAngles.y % 360) == 180)
+        {
+            player1_ViewFromBlueCamera_Transform.localPosition = redPortal_Transform.TransformDirection(redPortal_Position - player1_Position);
+            Debug.Log("90 - 180");
+        }
+        else if (Mathf.Abs(redPortal_Transform.eulerAngles.y % 360) == 270)
+        {
+            player1_ViewFromBlueCamera_Transform.localPosition = -redPortal_Transform.TransformDirection(redPortal_Position - player1_Position);
+            Debug.Log("180 - 270");
+        }
+        else if (Mathf.Abs(redPortal_Transform.eulerAngles.y % 360) == 360)
+        {
+            player1_ViewFromBlueCamera_Transform.localPosition = redPortal_Transform.TransformDirection(redPortal_Position - player1_Position);
+            Debug.Log("270 - 0");
+        }
+        
+
+
+
+
+
+        //Debug.Log(player1_Transform.eulerAngles + "+" + bluePortal_Transform.eulerAngles + "+" + redPortal_Transform.eulerAngles);
+
+        //player1_ViewFromBlueCamera_Transform
+
+
+
+        //bluePortal_Transform.localPosition + (redPortal_Transform.TransformDirection(redPortal_Position - player1_Position));
+
+
+
+
+        //player1_ViewFromBlueCamera_Transform.position = bluePortal_Transform.position;
+        //Vector3 newRot = bluePortal_Transform.eulerAngles;
+        //player1_ViewFromBlueCamera_Transform.eulerAngles = new Vector3(newRot.x, newRot.y, newRot.z);
+        //Vector3 newPos = redPortal_Transform.TransformDirection(player1_Position - redPortal_Position);
+        //player1_ViewFromBlueCamera_Transform.localPosition -= new Vector3(newPos.x, newPos.y, newPos.z);
+        /////////////player1_ViewFromBlueCamera_Transform.eulerAngles = player1_Transform.eulerAngles + (bluePortal_Transform.eulerAngles - redPortal_Transform.eulerAngles) + new Vector3(0, 180, 0);
+
         #endregion
     }
 }
